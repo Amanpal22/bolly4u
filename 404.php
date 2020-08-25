6e80a67c1af3bc8ad4d4e2b2dd8d3d3c3b69c6e4
Amanpal22 <70188379+Amanpal22@users.noreply.github.com>
2020-08-25 09:56:28 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php 
+$action=$_REQUEST['action'];
+$title = ''.$site.'';
+include 'includes/config.php';
+?>
+<div class=""> <div class="ad1"> <font color="red"><b>Page Not Found:</b></font> Page Not Found Please Return <a href="/">Home Page</a>.</div></div>
+<?php
+include 'includes/foot.php';
+?>
